885051ec49e351699b227c0d75be071e1beb1077
Daniel Corbu <dlcbx33@gmail.com>
2022-06-08 17:29:11 +0100

    dev: formulaire de connexion

--- front/PhotoBook/src/screens/LoginScreen.tsx
+++ front/PhotoBook/src/screens/LoginScreen.tsx
@@ -1,6 +1,6 @@
 import {  NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { RootStackParamList } from '../navigation';
 
 type LoginProps = NativeStackScreenProps<RootStackParamList, 'Login'>;
@@ -8,7 +8,9 @@ type LoginProps = NativeStackScreenProps<RootStackParamList, 'Login'>;
 const LoginScreen = ({navigation} : LoginProps) => { 
     return(
         <View style={styles.mainContainer}>
-          <Text style={styles.grosTexte}>LoginScreen !</Text>
+          <Text style={styles.grosTexte}>Faut se logguer!</Text>
+          <TextInput style={styles.lesLogins} placeholder="Login"  />
+          <TextInput style={styles.lesLogins} placeholder="Password" secureTextEntry />
           <Button title="Connect" onPress={() => 
             {console.log('coucou petite Merluche'); 
             navigation.navigate('Home');}
@@ -19,14 +21,24 @@ const LoginScreen = ({navigation} : LoginProps) => {
 };
 const styles = StyleSheet.create({
   mainContainer: {
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
     backgroundColor: '#003b6f',
-    height: '100%'
+    height: '100%',
+    padding : 5
   },
   grosTexte: {
     color: 'white',
     fontSize: 40
-  }
+  },
+  lesLogins: {
+    margin: 12,
+    borderWidth : 1,
+    borderColor: 'white',
+    borderRadius: 50,
+    padding: 10,
+    color : '#6A700B',
+    height: 40
+  },
 });
 export default LoginScreen;
